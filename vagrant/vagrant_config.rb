5c038fdfe89cef98f0ee902c5986d3097a6d103e
Min Pae <sputnik13@gmail.com>
2016-09-24 20:04:00 -0700

    added default vagrant_config.rb

---
+++ vagrant/vagrant_config.rb
@@ -0,0 +1,30 @@
+# -*- mode: ruby -*-
+# # vi: set ft=ruby :
+
+# Set $hostname to specify an explicit hostname for the VM
+$hostname = File.basename(File.expand_path(File.dirname(__FILE__))).delete('^A-Za-z0-9')
+
+$vm_count = 1
+
+$default_flavor = 'ubuntu'
+
+# Setup a guest port => host port mapping for the mappings provided below
+$forwarded_ports = {}
+
+# Ubuntu box
+$ubuntu_box = "sputnik13/trusty64"
+
+# Fedora box
+$fedora_box = "box-cutter/fedora20"
+
+# Specify a proxy to be used for packages
+$package_proxy = nil
+
+# Set $install_tmate to true to
+$install_tmate = false
+
+# Set the amount of RAM configured for the VM
+$vm_memory = 4096
+
+# Set the number of CPU cores configured for the VM
+$vm_cpus = 2
